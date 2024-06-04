0eac6520cca7c86a9f92fbbcb7a04b025f37cda0
luisapegoraro <luisa.pegoraro@gmail.com>
2024-06-04 18:22:01 -0400

    [isa] terminated eb instance

--- application.py
+++ application.py
@@ -26,3 +26,16 @@ if __name__ == "__main__":
 # eb deploy
 # eb open 
 # THIS TERMINATES THE ENVT: eb terminate flask-env
+
+
+# The environment "word-hide-env" and all associated instances will be terminated.
+# To confirm, type the environment name: word-hide-env
+# 2024-05-15 00:15:38    INFO    terminateEnvironment is starting.
+# 2024-05-15 00:15:38    INFO    Validating environment's EC2 instances have termination protection disabled before performing termination.
+# 2024-05-15 00:15:39    INFO    Finished validating environment's EC2 instances for termination protection.
+# 2024-05-15 00:15:56    INFO    Deleted CloudWatch alarm named: awseb-e-ertczum9cd-stack-AWSEBCloudwatchAlarmHigh-hV8uoo5RtIId 
+# 2024-05-15 00:15:56    INFO    Deleted CloudWatch alarm named: awseb-e-ertczum9cd-stack-AWSEBCloudwatchAlarmLow-GtyBvhtkXSvj 
+# 2024-05-15 00:15:56    INFO    Deleted Load Balancer listener named: arn:aws:elasticloadbalancing:us-west-2:076464304553:listener/app/awseb--AWSEB-UhNEU3B03lA1/acbbfbc719a58bbc/8494c6229b155b06
+# 2024-05-15 00:15:56    INFO    Deleted Auto Scaling group policy named: arn:aws:autoscaling:us-west-2:076464304553:scalingPolicy:669b0cab-5c11-4d03-af0a-2ffdaec64669:autoScalingGroupName/awseb-e-ertczum9cd-stack-AWSEBAutoScalingGroup-Vg0RZxfNtFOW:policyName/awseb-e-ertczum9cd-stack-AWSEBAutoScalingScaleUpPolicy-mImmCUyAvYsA
+# 2024-05-15 00:15:56    INFO    Deleted Auto Scaling group policy named: arn:aws:autoscaling:us-west-2:076464304553:scalingPolicy:08cd143d-9673-46ba-8857-4fef419e6c48:autoScalingGroupName/awseb-e-ertczum9cd-stack-AWSEBAutoScalingGroup-Vg0RZxfNtFOW:policyName/awseb-e-ertczum9cd-stack-AWSEBAutoScalingScaleDownPolicy-DHdXC2mzn99S
+# 2024-05-15 00:15:56    INFO    Waiting for EC2 instances to terminate. This may take a few minutes.
